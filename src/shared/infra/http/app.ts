9d5aedda79d318baff99036d865fbf682a1e523c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-13 19:57:17 -0300

    feat: add routes

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,10 +4,13 @@ import cors from 'cors';
 import express from 'express';
 
 import '../../../database';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(routes);
 
 export { app };
