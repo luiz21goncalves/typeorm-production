ceb8547d4ca1986410978599066782c48c55aae4
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-13 11:53:47 -0300

    feat: add typeorm

---
+++ ormconfig.example.js
@@ -0,0 +1,13 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'docker',
+  password: 'docker',
+  database: 'test',
+  entities: ['./src/entities/*.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations'
+  }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+import 'reflect-metadata'
+
 import express from 'express'
 import cors from 'cors'
 
 const app = express()
+
+import './database'
 
 app.use(express.json())
 app.use(cors())
