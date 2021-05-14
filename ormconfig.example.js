9f1c60a3c917836bc72c0c51e8212ffaf38ff98b
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-14 20:10:43 -0300

    feat: add build configurations

--- ormconfig.example.js
+++ ormconfig.example.js
@@ -1,5 +1,29 @@
 require('dotenv').config();
 
+const paths = {
+  production: {
+    entities: ['./dist/modules/**/entities/*.js'],
+    migrations: ['./dist/database/migrations/*.js'],
+    cli: {
+      migrationsDir: './dist/database/migrations',
+    },
+  },
+  development: {
+    entities: ['./src/modules/**/entities/*.ts'],
+    migrations: ['./src/database/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
+  },
+  test: {
+    entities: ['./src/modules/**/entities/*.ts'],
+    migrations: ['./src/database/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/database/migrations',
+    },
+  },
+};
+
 module.exports = {
   type: 'postgres',
   host: process.env.DATABASE_HOST,
@@ -7,9 +31,9 @@ module.exports = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: ['./src/modules/**/entities/*.ts'],
-  migrations: ['./src/database/migrations/*.ts'],
+  entities: paths[process.env.NODE_ENV].entities,
+  migrations: paths[process.env.NODE_ENV].migations,
   cli: {
-    migrationsDir: './src/database/migrations',
+    migrationsDir: paths[process.env.NODE_ENV].migrationsDir,
   },
 };
